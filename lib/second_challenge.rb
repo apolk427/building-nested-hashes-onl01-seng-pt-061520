e189ccf61a2a7d75fbf1fb8c71f2c6abcf4543f2
Amyre Polk <myremyre101@icloud.com>
2020-06-27 03:56:11 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,8 +6,8 @@ def second_challenge
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-     }, 
-    
+     },
+
     :capulet => {
       #your key/value pairs here!
       :patriarch  => {},
@@ -18,5 +18,5 @@ def second_challenge
      }
       
 
-  
+
 end
